9c2370f909fb3c16238864829b641d8a04a012f7
Ezequiel PD <ezeperez26@gmail.com>
2013-11-19 16:37:02 -0300

    Corrijo gateway de host C que está en el directorio de la entrega. Completo el informe con los pasos de la simulacion propuesto por Seba. Agrego el enunciado y anexo en el directorio Entrega y actualizo el PDF del Informe.

--- ENTREGA CD/Openvpn/Ejecutar en los clientes/configRuteoEstatico.sh
+++ ENTREGA CD/Openvpn/Ejecutar en los clientes/configRuteoEstatico.sh
@@ -7,7 +7,7 @@ sudo route add -net 0.0.0.0 gw 10.134.5.130 tap2 #DNS2
 sudo route add -net 0.0.0.0 gw 10.9.12.195 tap3 #DNS_ROOT
 sudo route add -net 0.0.0.0 gw 10.11.23.6 tap4 #Host A
 sudo route add -net 0.0.0.0 gw 10.134.1.4 tap5 #Host B
-sudo route add -net 0.0.0.0 gw 192.168.53.3 tap6 #Host C
+sudo route add -net 0.0.0.0 gw 10.9.12.193 tap6 #Host C
 sudo route add -net 0.0.0.0 gw 10.134.1.4 tap7 #Telnet 1
 sudo route add -net 0.0.0.0 gw 10.134.5.130 tap8 #Telnet 2
 sudo route add -net 0.0.0.0 gw 10.11.22.3 tap9 #Ftp
